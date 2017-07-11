9972467485f49f66fe257f94dbefe64a65915a9c
Troy Dai <troy.dai@outlook.com>
2017-07-10 23:11:43 -0700

    Print environment variables

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
 def root():
-    return 'Morocco - An automation service runs on Azure Batch.'
+    results = 'Morocco - An automation service runs on Azure Batch.\n'
+    for key in os.environ:
+        results += '{} = {}\n'.format(key, os.environ[key])
+    
+    return results
